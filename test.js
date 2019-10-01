003df60cdbf1d250637123fe76adb45c29fafd4c
Alexander Krenn <alexander.krenn@erfideo.com>
2019-10-01 13:07:30 +0200

    a

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-<script>
-    window.onload = function () {
-		alert(1);        
-    }
-</script>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script>
+    $( document ).ready(function() {
+		alert("1");
+        console.log( "document loaded" );
+    });
+ 
+    $( window ).on( "load", function() {
+		alert("1");
+        console.log( "window loaded" );
+    });
+    </script>

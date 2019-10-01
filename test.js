af1c0e566ee0a6e264c6647570aaab043a1ee0f7
Alexander Krenn <alexander.krenn@erfideo.com>
2019-10-01 13:15:58 +0200

    a

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-<script>
-function jsonpCallback(data){
-	console.dir(data);
-};
-
-var script = document.createElement('script');
-script.src = 'data.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-</script>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script>
+    $( document ).ready(function() {
+		alert("1");
+        console.log( "document loaded" );
+    });
+ 
+    $( window ).on( "load", function() {
+		alert("1");
+        console.log( "window loaded" );
+    });
+    </script>
